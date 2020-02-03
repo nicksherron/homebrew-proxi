643157d5208259f713a0973c33ac41a19be5782b
nicksherron <nsherron90@gmail.com>
2020-02-03 16:27:36 -0500

    release v0.1.2

--- Formula/proxi.rb
+++ Formula/proxi.rb
@@ -1,16 +1,16 @@
 class Proxi < Formula
   desc "Scrapes, checks and stores proxies with a rest api for querying results."
   homepage ""
-  version "0.1.1"
+  version "0.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nicksherron/proxi/releases/download/v0.1.1/proxi_v0.1.1_darwin_amd64.tar.gz"
-    sha256 "71cba7e0e5ed0d1c66ed172fe284d50e9e86f71b6b9a81a83f26a980beb9bbb7"
+    url "https://github.com/nicksherron/proxi/releases/download/v0.1.2/proxi_v0.1.2_darwin_amd64.tar.gz"
+    sha256 "3772ebe5220deb3c8c9bc70aa211b75b43517e06b8c6c11228883bbf18a1afed"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nicksherron/proxi/releases/download/v0.1.1/proxi_v0.1.1_linux_amd64.tar.gz"
-      sha256 "69bad08754bfc917d9236d93ec8d10294e686104a933a9c8e1ee23fc0faf244c"
+      url "https://github.com/nicksherron/proxi/releases/download/v0.1.2/proxi_v0.1.2_linux_amd64.tar.gz"
+      sha256 "71ab70fdf7fd8ab31f029cc2ca777e48e61275dd44b0f95dc0ee599c3a2fa6a8"
     end
   end
 
